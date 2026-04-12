f8c90dcc9ba67de0502820b8ed1b99fba11067fc
jayulkim <144874950+jayulkim@users.noreply.github.com>
2026-04-12 23:16:34 +0000

    Dijkstra

---
+++ Dijkstra.cpp
@@ -0,0 +1,62 @@
+class Dijkstra {
+private:
+	using ll = long long;
+	using pll = pair<ll, ll>;
+	ll n;
+	vector<vector<pll>>graph;
+public:
+	vector<ll>dist;
+	vector<vector<pll>>revgraph;
+	Dijkstra(ll n, const vector<vector<pll>>& graph) {
+		this->n = n;
+		this->graph = graph;
+	}
+	vector<ll> run(const vector<ll>& start) {
+		priority_queue<pll, vector<pll>, greater<pll>>pq;
+		dist.assign(n + 1, LLONG_MAX);
+		for (auto& i : start) {
+			dist[i] = 0;
+			pq.push({ 0, i });
+		}
+		while (!pq.empty()) {
+			auto [a, b] = pq.top();
+			pq.pop();
+			if (dist[b] >= a) {
+				for (auto& i : graph[b]) {
+					if (dist[i.first] > dist[b] + i.second) {
+						dist[i.first] = dist[b] + i.second;
+						pq.push({ dist[i.first], i.first });
+					}
+				}
+			}
+		}
+		return dist;
+	}
+	pair<vector<ll>, vector<vector<pll>>> revrun(const vector<ll>& start) {
+		priority_queue<pll, vector<pll>, greater<pll>>pq;
+		dist.assign(n + 1, LLONG_MAX);
+		revgraph.assign(n + 1, vector<pll>());
+		for (auto& i : start) {
+			dist[i] = 0;
+			pq.push({ 0, i });
+		}
+		while (!pq.empty()) {
+			auto [a, b] = pq.top();
+			pq.pop();
+			if (dist[b] >= a) {
+				for (auto& i : graph[b]) {
+					if (dist[i.first] > dist[b] + i.second) {
+						vector<pll>().swap(revgraph[i.first]);
+						revgraph[i.first].push_back({ b, i.second });
+						dist[i.first] = dist[b] + i.second;
+						pq.push({ dist[i.first], i.first });
+					}
+					else if (dist[i.first] == dist[b] + i.second) {
+						revgraph[i.first].push_back({ b, i.second });
+					}
+				}
+			}
+		}
+		return { dist, revgraph };
+	}
+};
